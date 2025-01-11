f601a600cf8908b36b5eb831abb3549afe951be9
stefano <stef.pro@hotmail.com>
2025-01-11 11:55:57 -0500

    fix: lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,11 @@
-import { m, useScroll, useSpring } from 'framer-motion';
-// next
-import Head from 'next/head';
-// @mui
-import { useTheme } from '@mui/material/styles';
-import { Box } from '@mui/material';
-// layouts
-import MainLayout from '../layouts/main';
-// sections
-import {
-  HomeHero,
-  HomeMinimal,
-  HomeDarkMode,
-  HomeLookingFor,
-  HomeForDesigner,
-  HomeColorPresets,
-  HomePricingPlans,
-  HomeAdvertisement,
-  HomeCleanInterfaces,
-  HomeHugePackElements,
-} from '../sections/home';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { PATH_AUTH } from 'src/routes/paths';
+// next
+import Head from 'next/head';
 import LoadingScreen from 'src/components/loading-screen/LoadingScreen';
+import { PATH_AUTH } from 'src/routes/paths';
+// layouts
+import MainLayout from '../layouts/main';
 
 // ----------------------------------------------------------------------
 
@@ -31,36 +14,11 @@ HomePage.getLayout = (page: React.ReactElement) => <MainLayout> {page} </MainLay
 // ----------------------------------------------------------------------
 
 export default function HomePage() {
-  const theme = useTheme();
   const router = useRouter();
-
-  const { scrollYProgress } = useScroll();
-
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
-
-  const progress = (
-    <m.div
-      style={{
-        top: 0,
-        left: 0,
-        right: 0,
-        height: 3,
-        zIndex: 1999,
-        position: 'fixed',
-        transformOrigin: '0%',
-        backgroundColor: theme.palette.primary.main,
-        scaleX,
-      }}
-    />
-  );
 
   useEffect(() => {
     router.replace(PATH_AUTH.login);
-  }, []);
+  }, [router]);
 
   return (
     <>
@@ -69,36 +27,6 @@ export default function HomePage() {
       </Head>
 
       <LoadingScreen />
-
-      {/* {progress}
-
-      <HomeHero />
-
-      <Box
-        sx={{
-          overflow: 'hidden',
-          position: 'relative',
-          bgcolor: 'background.default',
-        }}
-      >
-        <HomeMinimal />
-
-        <HomeHugePackElements />
-
-        <HomeForDesigner />
-
-        <HomeDarkMode />
-
-        <HomeColorPresets />
-
-        <HomeCleanInterfaces />
-
-        <HomePricingPlans />
-
-        <HomeLookingFor />
-
-        <HomeAdvertisement />
-      </Box> */}
     </>
   );
 }
